ac7b02b4a37e8c4ca30a305ba451f2162fd35543
Rhett Newton <rhett.newton@gmail.com>
2016-11-25 17:11:17 -0600

    add 01_temperature solution

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+def ftoc(f_temp)
+	#c_temp = (f_temp - 32) / 1.8
+	return ((f_temp - 32) / 1.8).round(2)
+end
+
+def ctof(c_temp)
+	#f_femp = c_temp * 1.8 + 32
+	return ((c_temp * 1.8) + 32).round(2)
+end
