238003f5f22f700428e3c1578d75c47ce9196b05
Rhett Newton <rhett.newton@gmail.com>
2016-11-26 11:57:54 -0600

    added 04_pig_latin solution

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,21 @@
-#write your code here
+def translate(str)
+	str = str.split
+	str.map! do |word|
+		if(word.start_with?('a', 'e', 'i', 'o', 'u'))
+			word += 'ay'
+		elsif(word.start_with?('squ', 'thr', 'sch'))
+			phoneme = word[0, 3]
+			word = word[3, word.length]
+			word += phoneme + 'ay'
+		elsif(word.start_with?('ch', 'qu', 'ph', 'th', 'br'))
+			phoneme = word[0, 2]
+			word = word[2, word.length]
+			word += phoneme + 'ay'
+		else
+			phoneme = word[0]
+			word = word[1, word.length]
+			word += phoneme + 'ay'
+		end
+	end
+	return str.join(' ')
+end
