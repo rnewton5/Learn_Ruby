04240ca63bbaad9223f723158f387fdb2c67f75e
Rhett Newton <rhett.newton@gmail.com>
2016-11-26 10:41:52 -0600

    added 02_calculator solution and updated calculator_spec.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,42 @@
-#write your code here
+def add(op_1, op_2)
+	return op_1 + op_2
+end
+
+def subtract(op_1, op_2)
+	return op_1 - op_2
+end
+
+def sum(array)
+	sum = 0
+	array.each do |num|
+		sum += num
+	end
+	return sum
+end
+
+def multiply(*args)
+	product = 1
+	args.each do |num|
+		product *= num
+	end
+	return product
+end
+
+def power(operand, power)
+	return operand ** power
+	#product = 1
+	#power.times do
+	#	product *= operand
+	#end
+	#return product
+end
+
+def factorial(number)
+	next_num = 1
+	product = 1
+	number.times do
+		product = product * next_num
+		next_num += 1
+	end
+	return product
+end
