a115f5ecacd6150019cb17b5fcdcb58d63321264
Rhett Newton <rhett.newton@gmail.com>
2016-11-27 08:30:58 -0600

    added 06_timer solution

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+  	@seconds = 0
+  end
+
+  def time_string
+  	seconds = @seconds
+  	minutes = seconds / 60
+  	hours = minutes / 60
+  	seconds = seconds % 60
+  	minutes = minutes % 60
+  	str = "%02d" % hours + ":"
+  	str += "%02d" % minutes + ":"
+  	str += "%02d" % seconds
+  	return str
+  end
 end
