d698b4164e3688445d89879753ddbd24f3a11971
Rhett Newton <rhett.newton@gmail.com>
2016-11-27 08:15:24 -0600

    added 05_book_titles solution

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+	def title=(str)
+		str = str.split
+		str.each do |word|
+			if(not ("and" "over" "the""of" "in").include?(word))
+				word.capitalize!
+			end
+			if(word == "i")
+				word.capitalize!
+			end
+		end
+		str = str.join(' ')
+		str[0] = str[0].capitalize
+		@title = str
+	end
 end
